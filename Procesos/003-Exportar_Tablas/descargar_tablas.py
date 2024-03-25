b17e90b1998ce6a5c35be88a593dcaad6088115e
Franco Schiavoni <francoschiavo7@gmail.com>
2024-03-25 08:27:43 -0300

    Export Tables
    test - ok

--- Procesos/003-Exportar_Tablas/descargar_tablas.py
+++ Procesos/003-Exportar_Tablas/descargar_tablas.py
@@ -8,25 +8,49 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..',
 import connector as s
 
 SAPtables = ["EQUI","EGERH"]
-folderdir = "C:\\_tmp\\"
+cantidadRegistros = "500"
 
 sap = s.SapConnector()
 resultados = []
 
 
-output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'Outputs'))
-print(output_folder)
+
+#Output Folder 
+output_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'Outputs/descargar_tablas'))
+
+
 now = datetime.now()
 date_string = now.strftime("-%Y%m%d_%H%M%S")
+
+def AplicarFiltro(column, value):
+    sap.session.findById("wnd[0]/usr/ctxtGD_ADD_COLUMN").text = column
+    sap.session.findById("wnd[0]/usr/ctxtGD_ADD_COLUMN").setFocus()
+    sap.session.findById("wnd[0]/usr/ctxtGD_ADD_COLUMN").caretPosition = 8
+    sap.session.findById("wnd[0]").sendVKey(0)
+    sap.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").text = value
+    sap.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").setFocus()
+    sap.session.findById("wnd[0]/usr/tblSAPLSE16NSELFIELDS_TC/ctxtGS_SELFIELDS-LOW[2,1]").caretPosition = 2
+    sap.session.findById("wnd[0]").sendVKey(0)
+
+sap.StartTransaction("/nSE16N")
+
+
+
+
 
 
 # Ingresa Tabla
 
 for table in SAPtables:
     sap.session.findById("wnd[0]/usr/ctxtGD-TAB").text = table
-    sap.session.findById("wnd[0]/usr/txtGD-MAX_LINES").text = "50"
+    sap.session.findById("wnd[0]/usr/txtGD-MAX_LINES").text = cantidadRegistros
     sap.session.findById("wnd[0]/usr/txtGD-MAX_LINES").setFocus()
     sap.session.findById("wnd[0]/usr/txtGD-MAX_LINES").caretPosition = 4
+    sap.session.findById("wnd[0]").sendVKey(0)
+    if table == "EQUI":
+        AplicarFiltro("MATNR", "700034")
+    elif table == "EGERH":
+        AplicarFiltro("GERWECHS", "05")
     sap.session.findById("wnd[0]/tbar[1]/btn[8]").press()
     sap.session.findById("wnd[0]/usr/cntlRESULT_LIST/shellcont/shell").pressToolbarContextButton("&MB_EXPORT")
     sap.session.findById("wnd[0]/usr/cntlRESULT_LIST/shellcont/shell").selectContextMenuItem("&XXL")
@@ -37,6 +61,8 @@ for table in SAPtables:
     sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
     sap.session.findById("wnd[0]/tbar[0]/btn[15]").press()
 
+
+sap.StartTransaction("/n")
 connection = None
 application = None
 SapGuiAuto = None
