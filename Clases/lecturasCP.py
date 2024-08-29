e3826b1a078e22d5910d5040f40c8142cf8cad9d
Lucas Fasolato <fasolatolucas14@gmail.com>
2024-08-29 10:44:54 -0300

    fix group by instalacion and nrocontrato, not finished

---
+++ Clases/lecturasCP.py
@@ -0,0 +1,234 @@
+import os
+import shutil
+import glob
+
+class LecturasCP:
+  
+  def __init__(self,sap):
+    self.id = ""
+    self.sap = sap
+    self.ins = ""
+    self.cc = ""
+    self.tipo_cliente = ""
+    self.trxCreateOrd = "/nEL01"
+    self.motivo_Lectura = "01"
+    self.fecha_Ord_Lectura_Desde = "31.01.2023"
+    self.fecha_Ord_Lectura_Hasta = ""
+    self.trxCargaLecturas = "/nEL28"
+    self.lectura_Pot_P   = []
+    self.lectura_Pot_R   = []
+    self.lectura_Pot_V   = []
+    self.lectura_E_act_P = []
+    self.lectura_E_act_R = []
+    self.lectura_E_act_V = []
+    self.lectura_Pot_P2   = ""
+    self.lectura_Pot_R2   = ""
+    self.lectura_Pot_V2   = ""
+    self.lectura_E_act_P2 = ""
+    self.lectura_E_act_R2 = ""
+    self.lectura_E_act_V2 = ""
+    self.lectura_E_act_P3 = ""
+    self.lectura_E_act_R3 = ""
+    self.lectura_E_act_V3 = ""
+    self.lectura_E_act_P4 = ""
+    self.lectura_E_act_R4 = ""
+    self.lectura_E_act_V4 = ""
+    self.lectura_E_react = "7500"
+    self.trxCalculo = "/nEA00"
+    self.fecha_Calculo = "10.02.2023"
+    self.trxFactura = "/nEA19"
+    self.clave_rec = "240321-001"
+    self.doc_calculo = ""
+    self.doc_impresion = ""
+  
+  def CreaOrdenLectura(self):
+    self.sap.session.findById("wnd[0]/usr/radRELX1-ANLAGE_T").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtREL01-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/usr/ctxtREL01-ABLESGR").text = self.motivo_Lectura
+    self.sap.session.findById("wnd[0]/tbar[1]/btn[8]").press()
+    self.sap.session.findById("wnd[0]/usr/cntlD0100_CONTAINER/shellcont/shell").firstVisibleRow = 12
+    self.sap.session.findById("wnd[0]/usr/cntlD0100_CONTAINER/shellcont/shell").pressToolbarButton("&MB_FILTER")
+    self.sap.session.findById("wnd[1]/usr/subSUB_DYN0500:SAPLSKBH:0600/cntlCONTAINER1_FILT/shellcont/shell").currentCellRow = 3
+    self.sap.session.findById("wnd[1]/usr/subSUB_DYN0500:SAPLSKBH:0600/cntlCONTAINER1_FILT/shellcont/shell").selectedRows = "3"
+    self.sap.session.findById("wnd[1]/usr/subSUB_DYN0500:SAPLSKBH:0600/btnAPP_WL_SING").press()
+    self.sap.session.findById("wnd[1]/usr/subSUB_DYN0500:SAPLSKBH:0600/btn600_BUTTON").press()
+    self.sap.session.findById("wnd[2]/usr/ssub%_SUBSCREEN_FREESEL:SAPLSSEL:1105/ctxt%%DYN001-LOW").text = self.fecha_Ord_Lectura_Desde
+    self.sap.session.findById("wnd[2]/usr/ssub%_SUBSCREEN_FREESEL:SAPLSSEL:1105/ctxt%%DYN001-LOW").caretPosition = 10
+    self.sap.session.findById("wnd[2]/tbar[0]/btn[0]").press()
+    self.sap.session.findById("wnd[0]/usr/cntlD0100_CONTAINER/shellcont/shell").currentCellColumn = "ADATSOLL"
+    self.sap.session.findById("wnd[0]/usr/cntlD0100_CONTAINER/shellcont/shell").selectedRows = "0"
+    self.sap.session.findById("wnd[0]/usr/cntlD0100_CONTAINER/shellcont/shell").clickCurrentCell()
+    self.sap.session.findById("wnd[0]/tbar[1]/btn[19]").press()
+
+  def GenerarOrdenLecturaMasiva(self, fechas_Ordlecturas):
+    self.sap.session.findById("wnd[0]/usr/radRELX1-ANLAGE_T").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtREL01-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/usr/ctxtREL01-ABLESGR").text = self.motivo_Lectura
+
+    for fecha in fechas_Ordlecturas:
+      self.sap.session.findById("wnd[0]/usr/ctxtREL01-ADATSOLL").text = fecha
+      self.sap.session.findById("wnd[0]/usr/ctxtREL01-ZUORDDAT").text = fecha
+      self.sap.session.findById("wnd[0]/usr/ctxtREL01-ZUORDDAT").setFocus()
+      self.sap.session.findById("wnd[0]/usr/ctxtREL01-ZUORDDAT").caretPosition = 10
+      self.sap.session.findById("wnd[0]/tbar[1]/btn[8]").press() #Ejecutar
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[15]").press()    
+    
+  def CargaLecturasGD(self, mdt):
+    self.sap.session.findById("wnd[0]/usr/radRELX1-ANLAGE_T").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtREL28D-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[0]").press()
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").text = self.lectura_E_act_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").text = self.lectura_Pot_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,2]").text = self.lectura_E_act_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,3]").text = self.lectura_Pot_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,4]").text = self.lectura_E_act_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,5]").text = self.lectura_Pot_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,6]").text = self.lectura_E_react
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+    if mdt == "398":
+      self.sap.session.findById("wnd[0]/tbar[0]/btn[0]").press()
+      self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").text = "0"
+      self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").caretPosition = 1
+      self.sap.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+
+  def CargaLecturasCOOP(self):
+    self.sap.session.findById("wnd[0]/usr/radRELX1-ANLAGE_T").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtREL28D-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[0]").press()
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").text  = self.lectura_Pot_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").text  = self.lectura_Pot_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,2]").text  = self.lectura_E_react
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,3]").text  = self.lectura_E_act_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,4]").text  = self.lectura_E_act_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,5]").text  = self.lectura_E_act_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,6]").text  = self.lectura_E_act_R2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,7]").text  = self.lectura_E_act_P2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,8]").text  = self.lectura_E_act_V2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,9]").text  = self.lectura_E_act_R3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,10]").text = self.lectura_E_act_P3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,11]").text = self.lectura_E_act_V3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,12]").text = self.lectura_E_act_R4
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,13]").text = self.lectura_E_act_P4
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,14]").text = self.lectura_E_act_V4
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+  
+  def CargaLecturasPROSU(self,mdt):
+    self.sap.session.findById("wnd[0]/usr/radRELX1-ANLAGE_T").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtREL28D-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[0]").press()
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").text = self.lectura_E_act_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").text = self.lectura_Pot_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,2]").text = self.lectura_E_act_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,3]").text = self.lectura_Pot_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,4]").text = self.lectura_E_act_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,5]").text = self.lectura_Pot_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,6]").text = self.lectura_E_react
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,7]").text = self.lectura_E_act_R2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,8]").text = self.lectura_Pot_R2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,9]").text = self.lectura_E_act_P2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,10]").text = self.lectura_Pot_P2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,11]").text = self.lectura_E_act_V2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,12]").text = self.lectura_Pot_V2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,15]").text = self.lectura_E_act_P3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,16]").text = self.lectura_E_act_R3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT").verticalScrollbar.position = 3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,14]").text = self.lectura_E_act_V3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,14]").setFocus()
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,14]").caretPosition = 3
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+    if mdt == "398":
+      self.sap.session.findById("wnd[0]").sendVKey(0)
+      self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").text = "0"
+      self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").text = "0"
+      self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").setFocus()
+      self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").caretPosition = 1
+      self.sap.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+
+  def CargaLecturasPROSU390(self,mdt):
+    self.sap.session.findById("wnd[0]/usr/radRELX1-ANLAGE_T").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtREL28D-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[0]").press()
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,0]").text = self.lectura_E_act_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,1]").text = self.lectura_Pot_R
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,2]").text = self.lectura_E_act_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,3]").text = self.lectura_Pot_P
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,4]").text = self.lectura_E_act_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,5]").text = self.lectura_Pot_V
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,6]").text = self.lectura_E_react
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,7]").text = self.lectura_E_act_R2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,8]").text = self.lectura_Pot_R2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,9]").text = self.lectura_E_act_P2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,10]").text = self.lectura_Pot_P2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,11]").text = self.lectura_E_act_V2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,12]").text = self.lectura_Pot_V2
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,13]").text = self.lectura_E_act_P3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,14]").text = self.lectura_E_act_R3
+    self.sap.session.findById("wnd[0]/usr/tblSAPLEL01CONTROL_SINGENT/txtREABLD-ZWSTAND[6,15]").text = self.lectura_E_act_V3
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[11]").press()
+
+  
+  def GeneraCalculo(self, fecha_C):
+    self.sap.session.findById("wnd[0]/usr/radEBISID-ANLAGERAD").select()
+    self.sap.session.findById("wnd[0]/usr/ctxtEBISID-ANLAGE").text = self.ins
+    self.sap.session.findById("wnd[0]/usr/radEBISID-BITRIGRAD").select()
+    if (len(fecha_C) == 0):
+      self.sap.session.findById("wnd[0]/usr/ctxtEBISID-ABRDATS").text = self.fecha_Calculo
+    else: 
+      self.sap.session.findById("wnd[0]/usr/ctxtEBISID-ABRDATS").text = fecha_C
+    self.sap.session.findById("wnd[0]/usr/ctxtEBISID-ABRDATS").setFocus()
+    self.sap.session.findById("wnd[0]/usr/ctxtEBISID-ABRDATS").caretPosition = 10
+    self.sap.session.findById("wnd[0]/tbar[1]/btn[8]").press()
+
+
+  def DescargarFactura(self, path_destino):
+    self.sap.session.findById("wnd[1]/tbar[0]/btn[18]").press()
+    self.doc_impresion = self.sap.session.findById("wnd[0]/usr/tabsTAB_PRINTDOC/tabpCMD_HEAD/ssubSUB_PRINTDOC:SAPLE22A:0501/ctxtERDK-OPBEL").text
+    self.sap.session.findById("wnd[0]/tbar[1]/btn[24]").press()
+    self.sap.session.findById("wnd[1]/usr/ctxtSSFPP-TDDEST").text = "locl"
+    self.sap.session.findById("wnd[1]/usr/ctxtSSFPP-TDDEST").caretPosition = 4
+    self.sap.session.findById("wnd[1]/tbar[0]/btn[8]").press()
+    self.sap.session.findById("wnd[0]/tbar[0]/okcd").text = "PDF!"
+    self.sap.session.findById("wnd[0]").sendVKey(0)
+
+    carpeta_temporal = os.path.join(os.getenv('LOCALAPPDATA'), 'Temp')
+    print(carpeta_temporal)
+    
+    patron_pdf = os.path.join(carpeta_temporal, '*smart*.pdf')
+    archivos_pdf = glob.glob(patron_pdf)
+
+    if archivos_pdf:
+      ruta_pdf = archivos_pdf[0]
+      carpeta_destino = path_destino
+      nuevo_nombre = self.ins + "_" + self.doc_impresion + ".pdf"
+      ruta_destino = os.path.join(carpeta_destino, nuevo_nombre)
+      shutil.copy(ruta_pdf, ruta_destino)
+      print("PDF movido exitosamente a la carpeta de destino.")
+    else:
+      print("No se encontraron archivos PDF en la carpeta temporal.")
+
+    self.sap.session.findById("wnd[1]").close()
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[15]").press()
+    self.sap.session.findById("wnd[0]/tbar[0]/btn[15]").press()
+    self.sap.session.findById("wnd[1]").close()
+
+
+
+  def GenerarFactura(self, descargar, path_destino, fecha_C):
+    if (len(fecha_C) == 0):
+      self.sap.session.findById("wnd[0]/usr/ctxtBUDAT").text = self.fecha_Calculo
+    else:
+      self.sap.session.findById("wnd[0]/usr/ctxtBUDAT").text = fecha_C
+    self.sap.session.findById("wnd[0]/usr/ctxtFIKEY").text = self.clave_rec 
+    self.sap.session.findById("wnd[0]/usr/ctxtVKONT").text = self.cc
+    self.sap.session.findById("wnd[0]/usr/ctxtVKONT").setFocus()
+    self.sap.session.findById("wnd[0]/usr/ctxtVKONT").caretPosition = 9
+    self.sap.session.findById("wnd[0]/tbar[1]/btn[8]").press()
+    self.doc_calculo = self.sap.session.findById("wnd[1]/usr/lbl[24,1]").text
+    self.sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
+    if descargar:
+      self.DescargarFactura(path_destino)
+    else:
+      self.sap.session.findById("wnd[1]/tbar[0]/btn[18]").press()
+      self.doc_impresion = self.sap.session.findById("wnd[0]/usr/tabsTAB_PRINTDOC/tabpCMD_HEAD/ssubSUB_PRINTDOC:SAPLE22A:0501/ctxtERDK-OPBEL").text
+      self.sap.session.findById("wnd[0]/tbar[0]/btn[15]").press()
+      self.sap.session.findById("wnd[1]/tbar[0]/btn[0]").press()
--- Procesos/005-Carga_Lecturas/carga_lecturas.py
+++ Procesos/005-Carga_Lecturas/carga_lecturas.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 import os
 
-file_path = (r"C:\Users\lfasolato\Desktop\TT_Estacionales_SAP.csv")
+file_path = (r"C:\Users\lfasolato\Desktop\TT_Estacionales_SAP_CSV.csv")
 
 if os.path.exists(file_path):
     # df = pd.read_csv(file_path, encoding='ISO-8859-1', header=0)
-    df = pd.read_csv(file_path, encoding='ISO-8859-1', header=1)
+    df = pd.read_csv(file_path, header=1, decimal=',')
     print("Archivo leído correctamente.")
 else:
     print(f"El archivo no existe en la ruta: {file_path}")
@@ -16,23 +16,26 @@ print(df.columns)
 
 import json
 
-# Limpia los valores faltantes en 'Anio' y 'Mes' reemplazándolos con 0 (o con un valor apropiado)
-df['Anio'] = df['Anio'].fillna(0).astype(int)
-df['Mes'] = df['Mes'].fillna(0).astype(int)
+# Limpia los valores faltantes en 'Instalacion' y 'NroContrato' reemplazándolos con 0 (o con un valor apropiado)
+df['Instalacion'] = df['Instalacion'].fillna(0).astype(int)
+df['NroContrato'] = df['NroContrato'].fillna(0).astype(int)
 
 # Asegúrate de que las columnas relevantes no contengan valores no numéricos
 df['Dem.Reg. Pico'] = pd.to_numeric(df['Dem.Reg. Pico'], errors='coerce').fillna(0)
 df['Dem.Reg. F.Pico'] = pd.to_numeric(df['Dem.Reg. F.Pico'], errors='coerce').fillna(0)
 
-# Agrupa por año
-grouped = df.groupby('Anio')
+# Agrupa por instalación y número de contrato
+grouped = df.groupby(['Instalacion', 'NroContrato'])
 
 result = {}
 
-for anio, group in grouped:
-    # Crea dos arrays de 12 espacios para cada año
+for (instalacion, nro_contrato), group in grouped:
+    # Crea dos arrays de 12 espacios para cada agrupación
     dem_reg_pico = [0] * 12
     dem_reg_fpico = [0] * 12
+    dem_reg_fpico2 = [0] * 12
+    dem_reg_energ_consu1 = [0] * 12
+    dem_reg_energ_consu2 = [0] * 12
 
     # Rellena los arrays con las lecturas del mes correspondiente
     for _, row in group.iterrows():
@@ -40,10 +43,28 @@ for anio, group in grouped:
         if 0 <= mes < 12:  # Verifica que el mes esté en el rango correcto
             dem_reg_pico[mes] = int(row['Dem.Reg. Pico'])
             dem_reg_fpico[mes] = int(row['Dem.Reg. F.Pico'])
+            dem_reg_fpico2[mes] = int(row['Dem.Reg. F.Pico'])
+            if(int(row['Dem.Reg. Pico']) > 3000 and int(row['Dem.Reg. Pico']) < 10000):
+                dem_reg_energ_consu1[mes] = 1500
+                dem_reg_energ_consu2[mes] = 2500
+            elif (int(row['Dem.Reg. Pico']) < 3000):
+                dem_reg_energ_consu1[mes] = 500
+                dem_reg_energ_consu2[mes] = 1500
+            elif (int(row['Dem.Reg. Pico']) > 10000):
+                dem_reg_energ_consu1[mes] = 4500
+                dem_reg_energ_consu2[mes] = 5500
 
-    result[anio] = {
+
+    # Estructura de la clave para el diccionario
+    key = f"inst-{instalacion}_cc-{nro_contrato}"
+
+    # Guarda los arrays en el resultado
+    result[key] = {
         'Dem_Reg_Pico': dem_reg_pico,
-        'Dem_Reg_F_Pico': dem_reg_fpico
+        'Dem_Reg_F_Pico': dem_reg_fpico,
+        'Dem_Reg_F_Pico2': dem_reg_fpico2,
+        'Dem_Reg_Energ_Consu1': dem_reg_energ_consu1,
+        'Dem_Reg_Energ_Consu2': dem_reg_energ_consu2
     }
 
 # Convierte el resultado a JSON
